e640c7c8fa08731610aeaceda6f13fa1e6a03e0a
Jay Vachon <jaylvachon@gmail.com>
2014-12-23 19:58:16 -0500

    add house

---
+++ dna-movement2/Assets/Scripts/Units/Static/HouseUnit.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HouseUnit : StaticUnit, IElderHoldable {
+
+	ElderHolder elders = new ElderHolder ();
+	public ElderHolder Elders {
+		get { return elders; }
+	}
+}

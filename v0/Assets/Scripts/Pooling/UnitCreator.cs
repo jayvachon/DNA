b8f01a32cf1fe2e7e685b50a42aa4a7b1c470642
Jay Vachon <jaylvachon@gmail.com>
2015-01-12 00:01:00 -0500

    add ice cream units

--- v0/Assets/Scripts/Pooling/UnitCreator.cs
+++ v0/Assets/Scripts/Pooling/UnitCreator.cs
@@ -3,7 +3,11 @@ using System.Collections;
 
 public class UnitCreator : MonoBehaviour {
 
-	void Awake () {
-		
+	void Start () {
+		ObjectPool.Instantiate ("Pasture", new Vector3 (0, 0.5f, 0));
+		ObjectPool.Instantiate ("Pasture", new Vector3 (-4, 0.5f, 4));
+		ObjectPool.Instantiate ("Pasture", new Vector3 (4, 0.5f, 4));
+		ObjectPool.Instantiate ("Pasture", new Vector3 (0, 0.5f, 8));
+		ObjectPool.Instantiate ("IceCreamCollector", new Vector3 (-2, 0.5f, -2));
 	}
 }
---
+++ v0/Assets/Scripts/Units/Movable/IceCreamCollectorUnit.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using GameInventory;
+using GameActions;
+
+public class IceCreamCollectorUnit : MovableUnit, IInventoryHolder, IActionable {
+
+	public Inventory Inventory { get; set; }
+	public ActionsList ActionsList { get; set; }
+
+	protected override void Awake () {
+		base.Awake ();
+		Inventory = new IceCreamCollectorInventory ();
+		ActionsList = new IceCreamCollectorActionsList (Inventory as IceCreamCollectorInventory);
+	}
+}
+
+public class IceCreamCollectorInventory : Inventory {
+
+	public IceCreamCollectorInventory () {
+		Add (new IceCreamHolder (5));
+	}
+}
+
+public class IceCreamCollectorActionsList : ActionsList {
+
+	public IceCreamCollectorActionsList (IceCreamCollectorInventory iceCreamCollectorInventory) {
+		Add (new CollectIceCream (iceCreamCollectorInventory));
+	}
+}

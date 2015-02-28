ec671f260e0de65eeba6bf82080f016f0a247fa1
Jay Vachon <jaylvachon@gmail.com>
2015-02-28 15:25:57 -0500

    better zoom on fractal

--- giving-tree/Assets/Scripts/GivingTree/3/FractalManager3.cs
+++ giving-tree/Assets/Scripts/GivingTree/3/FractalManager3.cs
@@ -7,20 +7,32 @@ public class FractalManager3 : MonoBehaviour {
 	public Transform cameraAnchor;
 	public GivingTree2 treePrefab;
 
-	int maxIterations = 3;
+	int maxIterations = 5;
 	List<GivingTree2> visitableTrees = new List<GivingTree2> ();
+
+	GivingTree2 FirstTree {
+		get { return visitableTrees[0]; }
+	}
+
+	GivingTree2 NextTree {
+		get { return visitableTrees[1]; }
+	}
 
 	GivingTree2 LastTree {
 		get { return visitableTrees[visitableTrees.Count-1]; }
 	}
 
 	int treeCount = 0;
+	bool iterating = false;
+	float targetScale;
 
 	void Awake () {
+		targetScale = transform.localScale.x / 10;
 		GivingTree2 targetTree = CreateTree (Vector3.zero, transform, true);
 		for (int i = 1; i < maxIterations; i ++) {
 			targetTree = CreateTree (targetTree.TargetTree.position, targetTree.transform, true);
 		}
+		cameraAnchor.SetPosition (LastTree.transform.position);
 	}
 
 	GivingTree2 CreateTree (Vector3 position, Transform parent, bool visitable, GivingTree2 tree=null) {
@@ -30,7 +42,7 @@ public class FractalManager3 : MonoBehaviour {
 		}
 
 		Transform treeTransform = tree.transform;
-		treeTransform.SetLocalPosition (position);
+		treeTransform.SetPosition (position);
 		treeTransform.SetParent (parent);
 		treeTransform.SetLocalScale (0.1f);
 		
@@ -44,11 +56,46 @@ public class FractalManager3 : MonoBehaviour {
 	}
 
 	void Iterate () {
-		GivingTree2 next = visitableTrees[0];
+		if (iterating) return;
+		iterating = true;
+		StartCoroutine (CoIterate ());
+	}
+
+	IEnumerator CoIterate () {
+		
+		float time = 1f;
+		float eTime = 0f;
+		Vector3 startScale = new Vector3 (0.1f, 0.1f, 0.1f);
+		Vector3 endScale = Vector3.one;
+		Vector3 startPosition = FirstTree.transform.position;
+		Vector3 endPosition = startPosition - (NextTree.transform.position / targetScale);
+
+		while (eTime < time) {
+			eTime += Time.deltaTime;
+			float progress = Mathf.SmoothStep (0, 1, eTime / time);
+			FirstTree.transform.localScale = Vector3.Lerp (
+				startScale,
+				endScale,
+				progress
+			);
+			FirstTree.transform.localPosition = Vector3.Lerp (
+				startPosition,
+				endPosition,
+				progress
+			);
+			yield return null;
+		}
+
+		iterating = false;
+		OnEndIterate ();
+	}
+
+	void OnEndIterate () {
+		GivingTree2 next = FirstTree;
 		visitableTrees.Remove (next);
-		visitableTrees[0].transform.SetParent (transform);
-		visitableTrees[0].transform.SetLocalScale (0.1f);
-		visitableTrees[0].transform.SetLocalPosition (Vector3.zero);
+		FirstTree.transform.SetParent (transform);
+		FirstTree.transform.SetLocalScale (0.1f);
+		FirstTree.transform.SetLocalPosition (Vector3.zero);
 		CreateTree (LastTree.TargetTree.position, LastTree.transform, true, next);
 	}
 

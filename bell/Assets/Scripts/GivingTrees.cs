64ed828e8550c43374e159de717f27fd9ad04e50
Jay Vachon <jaylvachon@gmail.com>
2015-03-29 22:34:45 -0400

    make bell tree:

---
+++ bell/Assets/Scripts/GivingTrees.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GivingTrees : MonoBehaviour {
+
+	public Transform givingTree;
+
+	Helix helix;
+
+	void Awake () {
+		helix = new Helix (10);
+		Vector3[] positions = helix.Positions;
+		Vector3[] rotations = helix.Rotations;
+		for (int i = 0; i < positions.Length; i ++) {
+			Transform newPlot = Instantiate (givingTree) as Transform;
+			newPlot.position = helix.Positions[i];
+			float scale = Mathf.Lerp (0.1f, 0f, (float)i / (float)positions.Length);
+			newPlot.localScale = new Vector3 (scale, scale, scale);
+			newPlot.localEulerAngles = rotations[i];
+			newPlot.SetParent (transform);
+		}
+	}
+}
---
+++ bell/Assets/Scripts/Pivot.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pivot : MonoBehaviour {
+
+	void Update () {
+		transform.Rotate (Vector3.up * 10f * Time.deltaTime);
+	}
+}

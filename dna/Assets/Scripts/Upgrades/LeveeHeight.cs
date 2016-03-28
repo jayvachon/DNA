4680bc85e2226819f9334be4e95f1caa83efff74
jayvachon <jaylvachon@gmail.com>
2016-03-27 20:46:23 -0400

    add levee height upgrade

---
+++ dna/Assets/Scripts/Upgrades/LeveeHeight.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeveeHeight : Upgrade<int> {
+
+	protected override int[] Levels {
+		get { return new int[] { 1, 3, 5, 7, 9, 11 }; }
+	}
+}

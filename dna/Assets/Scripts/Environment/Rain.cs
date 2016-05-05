a0913b1e74df683f07dec0fddeaf10f898c7206d
jayvachon <jaylvachon@gmail.com>
2016-05-05 17:01:01 -0400

    method 1 weather system

---
+++ dna/Assets/Scripts/Environment/Rain.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rain : MonoBehaviour {
+
+	ParticleSystem particles = null;
+	ParticleSystem Particles {
+		get {
+			if (particles == null) {
+				particles = GetComponent<ParticleSystem> ();
+			}
+			return particles;
+		}
+	}
+
+	[DebuggableMethod ()]
+	public void UpdateRain (float intensity) {
+		var em = Particles.emission;
+        em.rate = ParticleValue (0, 4000, intensity);
+	}
+
+	[DebuggableMethod ()]
+	public void UpdateWind (float intensity, float direction) {
+		Particles.startSpeed = Mathf.Lerp (40, 80, intensity);
+		var force = Particles.forceOverLifetime;
+		force.x = ParticleValue (Mathf.Cos (direction * Mathf.PI * 2) * intensity * 200);
+		force.y = ParticleValue (Mathf.Sin (direction * Mathf.PI * 2) * intensity * 200);
+	}
+
+	ParticleSystem.MinMaxCurve ParticleValue (float min, float max, float x) {
+		return new ParticleSystem.MinMaxCurve (Mathf.Lerp (min, max, x));
+	}
+
+	ParticleSystem.MinMaxCurve ParticleValue (float val) {
+		return new ParticleSystem.MinMaxCurve (val);
+	}
+}
--- dna/Assets/Scripts/Environment/WeatherManager.cs
+++ dna/Assets/Scripts/Environment/WeatherManager.cs
@@ -4,14 +4,67 @@ using System.Collections.Generic;
 
 public class WeatherManager : MonoBehaviour {
 
+	public Rain rain;
+
 	float stormThreshold = 0.67f;
-	int eventIndex = 0;
+
+	RainPattern rainPattern = new RainPattern ();
+	WindPattern windPattern = new WindPattern ();
 
 	void Update () {
-		Debug.Log (GetNextEvent ());
+		
+		rainPattern.Update ();
+		windPattern.Update ();
+
+		rain.UpdateRain (rainPattern.Intensity);
+		rain.UpdateWind (windPattern.Intensity, windPattern.Direction);
 	}
 
-	float GetNextEvent () {
-		return Mathf.PerlinNoise ((++eventIndex)*0.001f, 0);
+	// Method 1
+
+	class RainPattern {
+
+		WeatherPattern intensityPattern = new WeatherPattern (0.001f);
+
+		public float Intensity { get; private set; }
+
+		public void Update () {
+			Intensity = intensityPattern.Update ();
+		}
+	}
+
+	class WindPattern {
+
+		WeatherPattern intensityPattern = new WeatherPattern (0.01f);
+		WeatherPattern directionPattern = new WeatherPattern (0.001f);
+
+		public float Intensity { get; private set; }
+		public float Direction { get; private set; }
+
+		public void Update () {
+			Intensity = intensityPattern.Update ();
+			Direction = directionPattern.Update ();
+		}
+	}
+
+	class WeatherPattern {
+
+		public float Value { get; private set; }
+
+		int idx = 0;
+		float changeRate;
+		float threshold;
+
+		public WeatherPattern (float changeRate, float threshold=0.1f) {
+			this.changeRate = changeRate;
+			this.threshold = threshold;
+			// idx = Random.Range (0, 10000);
+		}
+
+		public float Update () {
+			Value = Mathf.Pow (Mathf.PerlinNoise ((++idx)*changeRate, 0), 4);
+			Value = Value >= threshold ? Value : 0f;
+			return Value;
+		}
 	}
 }

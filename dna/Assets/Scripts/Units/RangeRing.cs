9e305d5cd930f97590f12d1db029a1cd12c01691
jayvachon <jaylvachon@gmail.com>
2016-05-12 17:48:53 -0400

    display turret range on select

---
+++ dna/Assets/Scripts/Units/RangeRing.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace DNA.Units {
+
+	[RequireComponent (typeof (LineRenderer))]
+	public class RangeRing : MBRefs {
+
+		LineRenderer lineRenderer = null;
+		LineRenderer LineRenderer {
+			get {
+				if (lineRenderer == null) {
+					lineRenderer = GetComponent<LineRenderer> ();
+				}
+				return lineRenderer;
+			}
+		}
+
+		public void Show () {
+			LineRenderer.enabled = true;
+		}
+
+		public void Hide () {
+			LineRenderer.enabled = false;
+		}
+
+		public void Set (float radius, int resolution) {
+
+			Vector3[] positions = new Vector3[resolution+1];
+			float twopi = Mathf.PI * 2f;
+
+			LineRenderer.SetVertexCount (resolution+1);
+
+			for (int i = 0; i < resolution; i ++) {
+				float p = (float)i/(float)(resolution-1);
+				positions[i] = new Vector3 (radius * Mathf.Sin (twopi * p), 0f, radius * Mathf.Cos (twopi * p));
+			}
+
+			positions[positions.Length-1] = positions[0];
+
+			LineRenderer.SetPositions (positions);
+		}
+
+		public static RangeRing Create (Transform parent) {
+			RangeRing rangeRing = ObjectPool.Instantiate<RangeRing> ();
+			rangeRing.transform.SetParent (parent);
+			rangeRing.transform.Reset ();
+			return rangeRing;
+		}
+	}
+}
--- dna/Assets/Scripts/Units/Static/Turret.cs
+++ dna/Assets/Scripts/Units/Static/Turret.cs
@@ -5,5 +5,24 @@ namespace DNA.Units {
 
 	public class Turret : StaticUnit {
 		
+		float range = 10f;
+		RangeRing ring;
+
+		protected override void OnEnable () {
+			base.OnEnable ();
+			ring = RangeRing.Create (MyTransform);
+			ring.Set (range, 40);
+			ring.Hide ();
+		}
+
+		public override void OnSelect () {
+			base.OnSelect ();
+			ring.Show ();
+		}
+
+		public override void OnUnselect () {
+			base.OnUnselect ();
+			ring.Hide ();
+		}
 	}
 }
